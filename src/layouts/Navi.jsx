d795c897282d59d5a35a2e05baee1511d201a2fc
Melisa Yasmin Katrancı <179725887+qatmelya@users.noreply.github.com>
2022-06-21 01:37:21 +0300

    basic Sign In and Sign Out functionality added

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import CartSummary from './CartSummary';
 import SignedIn from './SignedIn';
 import SignedOut from './SignedOut';
 import { Button, Container, Menu } from 'semantic-ui-react';
 
 export default function Navi() {
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+  function handleSignOut() {
+    setisAuthenticated(false);
+  }
+  function handleSignIn() {
+    setisAuthenticated(true);
+  }
   return (
     <div>
       <Menu inverted fixed="top">
@@ -14,8 +22,11 @@ export default function Navi() {
 
           <Menu.Menu position="right">
             <CartSummary></CartSummary>
-            <SignedOut></SignedOut>
-            <SignedIn></SignedIn>
+            {isAuthenticated ? (
+              <SignedIn signOut={handleSignOut}></SignedIn>
+            ) : (
+              <SignedOut signIn={handleSignIn}></SignedOut>
+            )}
           </Menu.Menu>
         </Container>
       </Menu>
--- src/layouts/SignedOut.jsx
+++ src/layouts/SignedOut.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button, Menu } from 'semantic-ui-react';
 
-export default function SignedOut() {
+export default function SignedOut({ signIn }) {
   return (
     <div>
       <Menu.Item>
-        <Button primary>Sign In</Button>
+        <Button primary onClick={signIn}>
+          Sign In
+        </Button>
         <Button primary style={{ marginLeft: '0.2em' }}>
           Register
         </Button>
