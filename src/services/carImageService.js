9792357abb19f1ebb22cadd34cb79afa8b565f00
Melisa Yasmin Katrancı <179725887+qatmelya@users.noreply.github.com>
2022-06-22 15:30:36 +0300

    remove unnecesary declaration

--- src/services/carImageService.js
+++ src/services/carImageService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { carImageApi } from './serverUrl';
 export default class CarImageService {
-  apiPoint = `/api/carImages`;
   getByCarId(carId) {
     return axios.get(`${carImageApi}/getallbycarid?carid=${carId}`);
   }
